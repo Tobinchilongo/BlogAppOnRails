248a4e97c68163dc47acc1dc5343daed240900f1
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-06-08 16:59:30 +0200

    Implemented Changes suggested by the reviewer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  after_save :increment_posts_counter
+  def increment_posts_counter
+    author.increment!(:post_counter)
+  end
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes, dependent: :destroy
